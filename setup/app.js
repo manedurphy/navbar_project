548b817fe10674d4f0cdda68a7596a147faf95d0
Dane Murphy <danem7@gmail.com>
2020-07-06 15:56:43 -0700

    toggle bar functionality added with .toggle method. if-else statements commented out

--- setup/app.js
+++ setup/app.js
@@ -3,3 +3,21 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+//setting up variables for the nav-toggle button as well as each link
+const navToggle = document.querySelector('.nav-toggle');
+const links = document.querySelector('.links');
+
+//adding click events on navToggle
+navToggle.addEventListener('click', function () {
+  // console.log(links.classList);
+  // console.log(links.classList.contains('links'));
+
+  // if (links.classList.contains('show-links')) {
+  //   links.classList.remove('show-links')
+  // } else {
+  //   links.classList.add('show-links')
+  // }
+
+  links.classList.toggle('show-links');
+});
